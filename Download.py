1e397abff725978a464e1d8deef613dc6738ce72
SZD <324000115@qq.com>
2023-12-09 15:29:04 +0800

    控制台程序增加下载小于3M判断

--- Download.py
+++ Download.py
@@ -6,6 +6,19 @@ import Utils
 global file_path
 
 
+def check_and_remove_empty_folder(folder_path):
+    # 检查文件夹是否为空
+    if not os.listdir(folder_path):
+        # 如果为空，删除文件夹
+        os.rmdir(folder_path)
+        Utils.print_colored(f"删除'{folder_path}'文件夹!!!", "31")
+        # 同时检查并递归删除父文件夹
+        parent_folder = os.path.dirname(folder_path)
+        if not os.listdir(parent_folder):
+            os.rmdir(parent_folder)
+            Utils.print_colored(f"删除'{parent_folder}'文件夹!!!", "31")
+
+
 def get_file(indexes, data_array, root_dir, page_number):
     """
     下载指定索引的文件并保存到指定目录中
@@ -32,11 +45,18 @@ def get_file(indexes, data_array, root_dir, page_number):
                 file_path = Utils.download_file(file_url, author, title, root_dir)
 
                 print(f"第{page_number}页-第{index + 1}首-已下载文件: {file_path}")
+                # 判断文件大小
+                file_size = os.path.getsize(file_path)
+                if file_size < 3000000:  # 3M
+                    os.remove(file_path)
+                    Utils.print_colored(f"但是文件小于3M已经删除!!!", "31")
+                    # 检查并删除空文件夹
+                    check_and_remove_empty_folder(os.path.dirname(file_path))
             except requests.exceptions.HTTPError as e:
-                print(f"第{page_number}页-第{index + 1}首-下载文件失败: {e}")
+                Utils.print_colored(f"第{page_number}页-第{index + 1}首-下载文件失败: {e}", "31")
                 # 删除下载过程中产生的文件
                 if os.path.exists(file_path):
                     os.remove(file_path)
-                    print(f"第{page_number}页-第{index + 1}首-删除文件: {file_path}")
+                    Utils.print_colored(f"第{page_number}页-第{index + 1}首-删除文件: {file_path}", "31")
         else:
-            print(f"索引 {index + 1} 超出数据范围")
+            Utils.print_colored(f"索引 {index + 1} 超出数据范围", "31")
